c12288b8e6413558f5b1fd2bf925039ce4b3f328
eng-alecrim <lucasalecrimvieira@gmail.com>
2024-06-18 13:16:07 -0300

    add fluxo para download

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
+# =============================================================================
+# BIBLIOTECAS E MÓDULOS
+# =============================================================================
+
+import dotenv, os
 from ftplib import FTP
-from src.download import *
+from src.download import download_arquivos
 
-def main() -> None:
+# =============================================================================
+# CONSTANTES
+# =============================================================================
 
+# -----------------------------------------------------------------------------
+# Variáveis de ambiente
+# -----------------------------------------------------------------------------
+
+dotenv.load_dotenv()
+FTP_GOV_RAIZ = os.getenv("FTP_GOV_RAIZ")
+FTP_RAIS = os.getenv("FTP_RAIS")
+
+# =============================================================================
+# FUNÇÕES
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Download
+# -----------------------------------------------------------------------------
+
+
+def fluxo_download() -> None:
+    assert isinstance(FTP_GOV_RAIZ, str), "'FTP_GOV_RAIZ' NÃO ESTÁ NO ARQUIVO .env!"
     ftp = FTP(FTP_GOV_RAIZ)
     ftp.login()
+    assert isinstance(FTP_RAIS, str), "'FTP_RAIS' NÃO ESTÁ NO ARQUIVO .env!"
     ftp.cwd(FTP_RAIS)
     diretorios_anos = (dir for dir in ftp.nlst())
     for diretorio in diretorios_anos:
-        download_arquivos(ftp,diretorio)
+        download_arquivos(ftp, diretorio)
     ftp.quit()
-
     return None
+
+
+# =============================================================================
+# CÓDIGO
+# =============================================================================
+
+
+def main() -> None:
+    fluxo_download()
+    return None
+
 
 if __name__ == "__main__":
     main()
--- src/download.py
+++ src/download.py
@@ -1,35 +1,49 @@
+# =============================================================================
+# BIBLIOTECAS E MÓDULOS
+# =============================================================================
+
 from ftplib import FTP
 from pathlib import Path
+import dotenv, os
 
-DIR_DOWNLOAD = Path("data/bronze/rais")
-DIR_DOWNLOAD.mkdir(parents=True,exist_ok=True)
-FTP_GOV_RAIZ = "ftp.mtps.gov.br"
-FTP_RAIS = "pdet/microdados/RAIS"
+# =============================================================================
+# CONSTANTES
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Variáveis de ambiente
+# -----------------------------------------------------------------------------
+
+dotenv.load_dotenv()
+DIR_DOWNLOAD = os.getenv("DIR_DOWNLOAD")
+assert isinstance(DIR_DOWNLOAD, str), "'DIR_DOWNLOAD' NÃO ESTÁ NO ARQUIVO .env!"
+DIR_DOWNLOAD = Path(DIR_DOWNLOAD)
+DIR_DOWNLOAD.mkdir(parents=True, exist_ok=True)
+
+# =============================================================================
+# FUNÇÕES
+# =============================================================================
 
 
-def download_arquivos(ftp:FTP, diretorio_ano:str) -> None:
-    def download_arquivo(arquivo_download:str) -> None:
+def download_arquivos(ftp: FTP, diretorio_ano: str) -> None:
+    def download_arquivo(arquivo_download: str) -> None:
         path_download = diretorio_download / arquivo_download
 
         if path_download.exists():
             return None
 
         with open(path_download, "wb") as download_f:
-            ftp.retrbinary(
-                f"RETR {arquivo_download}", 
-                download_f.write
-            )
+            ftp.retrbinary(f"RETR {arquivo_download}", download_f.write)
 
         return None
 
     ftp.cwd(diretorio_ano)
 
+    assert isinstance(DIR_DOWNLOAD, Path)
     diretorio_download = DIR_DOWNLOAD / diretorio_ano
-    diretorio_download.mkdir(parents=True,exist_ok=True)
+    diretorio_download.mkdir(parents=True, exist_ok=True)
 
-    arquivos_download = (
-        arq for arq in ftp.nlst() if ".7z" in arq
-    )
+    arquivos_download = (arq for arq in ftp.nlst() if ".7z" in arq)
 
     for arquivo in arquivos_download:
         download_arquivo(arquivo)
